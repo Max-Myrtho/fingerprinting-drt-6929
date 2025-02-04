d00bff81ee6af87ffa186c8757ba68dab29e7cc2
Max-Myrtho <hello@maxmyrtho.me>
2025-02-03 21:03:55 -0500

    Correction du texte

--- src/Home.tsx
+++ src/Home.tsx
@@ -2,13 +2,11 @@ const Home = () => {
   return (
     <div className='mb-4'>
       <h2 className='text-center text-4xl mb-4 underline'>Démonstration du Fingerprinting (DRT-6929)</h2>
-      {/* <p>Ce site accompagne le texte <a href="#" target="_blank">[Titre du texte]</a> en proposant une présentation pratique du fingerprinting. 
-        Il a été réalisé dans le cadre du cours DRT-6929 à l'Université de Montréal.
-        Ce projet est en libre accès et disponible sur <a href="#" target="_blank">GitHub</a> sous licence MIT.</p> */}
-        <p>Ce site est une présentation pratique du fingerprinting. 
-        Il a été réalisé dans le cadre du cours DRT-6929 à l'Université de Montréal.
-        Ce projet est en libre accès et disponible sur <a href="https://github.com/Max-Myrtho/fingerprinting-drt-6929" target="_blank">GitHub</a> sous licence MIT.</p>
-        <a href="https://maxmyrtho.me" target="_blank">Max-Myrtho Saint-Louis</a>
+      {/* <p>Ce site accompagne le texte <a href="#" target="_blank">Le fingerprinting : une arme à double tranchant pour la vie privée.</a> en proposant une présentation pratique du fingerprinting. */}
+        <p>Ce site propose une présentation pratique du fingerprinting et accompagne un texte de blog. 
+          Il a été réalisé dans le cadre du cours DRT-6929 à l'Université de Montréal.
+          <br/><u>Aucune information personnelle n'est enregistrée.</u> Ce projet est en libre accès et disponible sur <a href="https://github.com/Max-Myrtho/fingerprinting-drt-6929" target="_blank">GitHub</a> sous licence MIT.</p>
+        - <a href="https://maxmyrtho.me" target="_blank">Max-Myrtho Saint-Louis</a> -
          <hr/>
     </div>
   )
